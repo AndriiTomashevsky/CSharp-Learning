8522ea3a51704b65b2b93b59db46cffa9b24a470
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-23 15:51:50 +0300

    Processing Command-Line Arguments, Get arguments using System.Environment.

--- SimpleCSharpApp/Program.cs
+++ SimpleCSharpApp/Program.cs
@@ -4,7 +4,7 @@ namespace SimpleCSharpApp
 {
     class Program
     {
-        // Note we are now returning an int, rather than void.
+        // Notice you have no need to check the size of the array when using "foreach".
         static int Main(string[] args)
         {
             // Display a message and wait for Enter key to be pressed.
@@ -12,9 +12,10 @@ namespace SimpleCSharpApp
             Console.WriteLine("Hello World!");
             Console.WriteLine();
 
-            // Process any incoming args.
-            for (int i = 0; i < args.Length; i++)
-                Console.WriteLine("Arg: {0}", args[i]);
+            // Get arguments using System.Environment.
+            string[] theArgs = Environment.GetCommandLineArgs();
+            foreach (string arg in theArgs)
+                Console.WriteLine("Arg: {0}", arg);
 
             Console.ReadLine();
             return -1;
